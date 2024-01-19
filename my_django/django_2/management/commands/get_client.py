50ebc26c6b231f7aa6296b2be8d16068a9e7312b
Yury <dr.jufereff@mail.ru>
2024-01-19 12:15:31 +0300

    django3

--- my_django/django_2/management/commands/get_client.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django_2.models import Client
-
-class Command(BaseCommand):
-    help = "Get client by id."
-
-def get_client(client_id):
-    client = Client.objects.get(id=client_id)
-    return client
